6036909e68d7e8e10c0ddc647b5bc07b9efc5f8e
phish108 <cpglahn@gmail.com>
2012-08-09 15:48:08 +0300

    Update jester.js

--- jester.js
+++ jester.js
@@ -7,6 +7,7 @@
  * Released under MIT License
  *
  * Copyright (C) 2011 by Scott Seaward
+ * Copyright (C) 2012 by Christian Glahn (Android Support Extensions)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
